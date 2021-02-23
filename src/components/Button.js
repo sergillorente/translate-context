8eee575ad51664a63563ad6f60481a35006ba8bb
[sergillorente] <[sergi.llorente.blasco@gmail.com]>
2021-02-23 12:25:14 +0100

    New components created + addition of semantic.ui. Basic structure of all of them

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Button extends Component {
+    render() {
+        return (
+            <button className="ui button primary">Submit</button>
+        )
+    }
+}
+
+export default Button;
