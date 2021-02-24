f15fc8f96efb36dbba9c3267cd7ca9ed0c744273
[sergillorente] <[sergi.llorente.blasco@gmail.com]>
2021-02-24 18:27:04 +0100

    Connecting the Selector to the Store

--- src/components/App.js
+++ src/components/App.js
@@ -6,19 +6,12 @@ import ColorContext from '../contexts/ColorContext';
 import LanguageSelector from './LanguageSelector';
 
 class App extends React.Component {
-    state = {
-        language: 'english'
-    };
-
-    onLanguageChange = language => {
-        this.setState({ language })
-    };
 
     render() {
         return (
             <div className="ui container">
                 <LanguageStore>
-                    <LanguageSelector onLanguageChange={this.onLanguageChange} />
+                    <LanguageSelector />
 
                         <ColorContext.Provider value="primary">
                             <UserCreate />
